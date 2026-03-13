3285ef9208a0cd12d0efc0178653fa2af6358d05
KavinduAnj <wkanjana1590@gmail.com>
2026-03-14 00:14:27 +0530

    feat: Add initial `App.jsx` component with basic layout, navigation, and search input.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,23 +6,27 @@ function App() {
 
   return (
     <>
-      <div className='min-h-screen'>
-        <nav className='flex items-center p-5 shadow-sm shadow-gray-200'>
+      <div className='min-h-screen bg-gray-100'>
+        <nav className='flex items-center p-4 shadow-sm shadow-gray-200'>
           <div className="flex-2 items-center gap-2">
-          <span className="text-xl font-bold tracking-tight">GitLearn</span>
+            <span className="text-xl font-bold tracking-tight">GitLearn</span>
           </div>
           <div className="flex items-center gap-6">
-          <button className="flex items-center gap-2 px-3 py-1.5 border border-gray-200 rounded-full text-sm font-medium hover:bg-gray-50 transition-colors">
-            <div className="w-3 h-3 bg-black rounded-full"></div>
-            Dark
-          </button>
-        </div>
+            <button className="flex items-center gap-2 px-3 py-1.5 border border-gray-200 rounded-full text-sm font-medium hover:bg-gray-50 transition-colors">
+              <div className="w-3 h-3 bg-black rounded-full"></div>
+              Dark
+            </button>
+          </div>
 
         </nav>
 
-        <div className='flex flex-col mt-40 gap-4'>
+        <div className='flex flex-col mt-30 gap-4'>
           <div className='text-8xl justify-center text-center font-bold font-serif'>Explore open-source</div>
           <div className='text-7xl text-center font-semibold font-serif'>Master the craft</div>
+          <p className='text-xl text-center  '>Curated github repositories</p>
+          <div className='flex justify-center p-5'>
+            <input type="text" placeholder='Search' className='w-1/2 p-3 border border-gray-300 rounded-full' />
+          </div>
 
         </div>
         <div className='flex justify-content items-center h-screen'>
